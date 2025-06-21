622fd4b6ed501d7e002385dbbd32c8f58f69303c
Stan Alexandru <61749830+AlexStan141@users.noreply.github.com>
2025-06-22 00:29:21 +0300

    React + Node Deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    response: ""
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("https://school-server-59er.onrender.com")
+    this.setState({response: response})
+  }
+
+  render(){
+    return <p>{response}</p>
+  }
+
 }
 
 export default App;
